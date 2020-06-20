8b89ecd0f0fa3b5b9ce4b933ff4a0be29f267c41
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-06-20 16:14:39 +0100

    4_HMM Tagger | helpers.py | reformat code

--- Projects/4_HMM Tagger/helpers.py
+++ Projects/4_HMM Tagger/helpers.py
@@ -1,33 +1,44 @@
-
 import os
-import matplotlib.pyplot as plt
-import matplotlib.image as mplimg
-import networkx as nx
 import random
-
+from collections import namedtuple
+from collections import OrderedDict
 from io import BytesIO
+
+import matplotlib.image as mplimg
+import matplotlib.pyplot as plt
+import networkx as nx
 from itertools import chain
-from collections import namedtuple, OrderedDict
+
 
 
 Sentence = namedtuple("Sentence", "words tags")
 
-def read_data(filename):
+
+def read_data( filename ):
     """Read tagged sentence data"""
     with open(filename, 'r') as f:
         sentence_lines = [l.split("\n") for l in f.read().split("\n\n")]
-    return OrderedDict(((s[0], Sentence(*zip(*[l.strip().split("\t")
-                        for l in s[1:]]))) for s in sentence_lines if s[0]))
+    return OrderedDict((
+        (
+            s[0],
+            Sentence(*zip(*[
+                l.strip().split("\t")
+                for l in s[1:]
+           ]))
+        )
+        for s in sentence_lines
+        if s[0]
+    ))
 
 
-def read_tags(filename):
+def read_tags( filename ):
     """Read a list of word tag classes"""
     with open(filename, 'r') as f:
         tags = f.read().split("\n")
     return frozenset(tags)
 
 
-def model2png(model, filename="", overwrite=False, show_ends=False):
+def model2png( model, filename="", overwrite=False, show_ends=False ):
     """Convert a Pomegranate model into a PNG image
 
     The conversion pipeline extracts the underlying NetworkX graph object,
@@ -58,7 +69,7 @@ def model2png(model, filename="", overwrite=False, show_ends=False):
     nodes = model.graph.nodes()
     if not show_ends:
         nodes = [n for n in nodes if n not in (model.start, model.end)]
-    g = nx.relabel_nodes(model.graph.subgraph(nodes), {n: n.name for n in model.graph.nodes()})
+    g = nx.relabel_nodes(model.graph.subgraph(nodes), { n: n.name for n in model.graph.nodes() })
     pydot_graph = nx.drawing.nx_pydot.to_pydot(g)
     pydot_graph.set_rankdir("LR")
     png_data = pydot_graph.create_png(prog='dot')
@@ -74,7 +85,7 @@ def model2png(model, filename="", overwrite=False, show_ends=False):
     return mplimg.imread(img_data)
 
 
-def show_model(model, figsize=(5, 5), **kwargs):
+def show_model( model, figsize=(5, 5), **kwargs ):
     """Display a Pomegranate model as an image using matplotlib
 
     Parameters
@@ -97,46 +108,50 @@ def show_model(model, figsize=(5, 5), **kwargs):
 
 
 class Subset(namedtuple("BaseSet", "sentences keys vocab X tagset Y N stream")):
-    def __new__(cls, sentences, keys):
+    def __new__( cls, sentences, keys ):
         word_sequences = tuple([sentences[k].words for k in keys])
-        tag_sequences = tuple([sentences[k].tags for k in keys])
-        wordset = frozenset(chain(*word_sequences))
-        tagset = frozenset(chain(*tag_sequences))
-        N = sum(1 for _ in chain(*(sentences[k].words for k in keys)))
-        stream = tuple(zip(chain(*word_sequences), chain(*tag_sequences)))
-        return super().__new__(cls, {k: sentences[k] for k in keys}, keys, wordset, word_sequences,
+        tag_sequences  = tuple([sentences[k].tags for k in keys])
+        wordset        = frozenset(chain(*word_sequences))
+        tagset         = frozenset(chain(*tag_sequences))
+        N              = sum(1 for _ in chain(*(sentences[k].words for k in keys)))
+        stream         = tuple(zip(chain(*word_sequences), chain(*tag_sequences)))
+        return super().__new__(cls, { k: sentences[k] for k in keys }, keys, wordset, word_sequences,
                                tagset, tag_sequences, N, stream.__iter__)
 
-    def __len__(self):
+
+    def __len__( self ):
         return len(self.sentences)
 
-    def __iter__(self):
+
+    def __iter__( self ):
         return iter(self.sentences.items())
 
 
 class Dataset(namedtuple("_Dataset", "sentences keys vocab X tagset Y training_set testing_set N stream")):
-    def __new__(cls, tagfile, datafile, train_test_split=0.8, seed=112890):
-        tagset = read_tags(tagfile)
-        sentences = read_data(datafile)
-        keys = tuple(sentences.keys())
-        wordset = frozenset(chain(*[s.words for s in sentences.values()]))
+    def __new__( cls, tagfile, datafile, train_test_split=0.8, seed=112890 ):
+        tagset         = read_tags(tagfile)
+        sentences      = read_data(datafile)
+        keys           = tuple(sentences.keys())
+        wordset        = frozenset(chain(*[s.words for s in sentences.values()]))
         word_sequences = tuple([sentences[k].words for k in keys])
-        tag_sequences = tuple([sentences[k].tags for k in keys])
+        tag_sequences  = tuple([sentences[k].tags for k in keys])
         N = sum(1 for _ in chain(*(s.words for s in sentences.values())))
-        
+
         # split data into train/test sets
         _keys = list(keys)
         if seed is not None: random.seed(seed)
         random.shuffle(_keys)
-        split = int(train_test_split * len(_keys))
+        split         = int(train_test_split * len(_keys))
         training_data = Subset(sentences, _keys[:split])
-        testing_data = Subset(sentences, _keys[split:])
-        stream = tuple(zip(chain(*word_sequences), chain(*tag_sequences)))
+        testing_data  = Subset(sentences, _keys[split:])
+        stream        = tuple(zip(chain(*word_sequences), chain(*tag_sequences)))
         return super().__new__(cls, dict(sentences), keys, wordset, word_sequences, tagset,
                                tag_sequences, training_data, testing_data, N, stream.__iter__)
 
-    def __len__(self):
+
+    def __len__( self ):
         return len(self.sentences)
 
-    def __iter__(self):
+
+    def __iter__( self ):
         return iter(self.sentences.items())
